f8317c3434802dc1b9f0c2b024e5d1f8507bb803
Python Engineer <pythonengineerman@gmail.com>
2020-04-17 09:31:56 +0200

    switched wrong file names for 06_1 and 06_2

--- 06_1_loss_and_optimizer.py
+++ 06_1_loss_and_optimizer.py
@@ -13,49 +13,31 @@ import torch.nn as nn
 
 # here : f = 2 * x
 
-# 0) Training samples, watch the shape!
-X = torch.tensor([[1], [2], [3], [4]], dtype=torch.float32)
-Y = torch.tensor([[2], [4], [6], [8]], dtype=torch.float32)
+# 0) Training samples
+X = torch.tensor([1, 2, 3, 4], dtype=torch.float32)
+Y = torch.tensor([2, 4, 6, 8], dtype=torch.float32)
 
-n_samples, n_features = X.shape
-print(f'#samples: {n_samples}, #features: {n_features}')
-# 0) create a test sample
-X_test = torch.tensor([5], dtype=torch.float32)
+# 1) Design Model: Weights to optimize and forward function
+w = torch.tensor(0.0, dtype=torch.float32, requires_grad=True)
 
-# 1) Design Model, the model has to implement the forward pass!
-# Here we can use a built-in model from PyTorch
-input_size = n_features
-output_size = n_features
+def forward(x):
+    return w * x
 
-# we can call this model with samples X
-model = nn.Linear(input_size, output_size)
-
-'''
-class LinearRegression(nn.Module):
-    def __init__(self, input_dim, output_dim):
-        super(LinearRegression, self).__init__()
-        # define diferent layers
-        self.lin = nn.Linear(input_dim, output_dim)
-    
-    def forward(self, x):
-        return self.lin(x)
-
-model = LinearRegression(input_size, output_size)
-'''
-
-print(f'Prediction before training: f(5) = {model(X_test).item():.3f}')
+print(f'Prediction before training: f(5) = {forward(5).item():.3f}')
 
 # 2) Define loss and optimizer
 learning_rate = 0.01
 n_iters = 100
 
+# callable function
 loss = nn.MSELoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+optimizer = torch.optim.SGD([w], lr=learning_rate)
 
 # 3) Training loop
 for epoch in range(n_iters):
-    # predict = forward pass with our model
-    y_predicted = model(X)
+    # predict = forward pass
+    y_predicted = forward(X)
 
     # loss
     l = loss(Y, y_predicted)
@@ -70,7 +52,6 @@ for epoch in range(n_iters):
     optimizer.zero_grad()
 
     if epoch % 10 == 0:
-        [w, b] = model.parameters() # unpack parameters
-        print('epoch ', epoch+1, ': w = ', w[0][0].item(), ' loss = ', l)
+        print('epoch ', epoch+1, ': w = ', w, ' loss = ', l)
 
-print(f'Prediction after training: f(5) = {model(X_test).item():.3f}')
+print(f'Prediction after training: f(5) = {forward(5).item():.3f}')
